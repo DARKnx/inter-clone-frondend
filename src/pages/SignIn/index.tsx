2b97f30eb0d2fdc92db3c414590a7d70286410dc
DARKnx <joao.vitornl@gmail.com>
2021-12-12 11:28:10 -0300

    iniciando pagina de login.

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
+import { Wrapper, Background, InputContainer, ButtonContainer } from './styles';
+import background  from '../../assets/images/background-login.jpg';
+import Card from '../../components/Card';
+
 const SignIn = () => {
     return (
-        <div>
-            SignIn
-        </div>
+
+        <Wrapper>
+         
+          <Background image={background}/>
+          <Card width='403px'>DARKnx</Card>
+        </Wrapper>
     )
 }
 
